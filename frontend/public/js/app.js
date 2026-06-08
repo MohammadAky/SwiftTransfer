fd35a7373e23e958e8ae222691b1c5b114cf0e98
Mohammad Akbary <mohammad_aky@yahoo.com>
2026-06-08 20:45:51 +0330

    add basic frontend to know onlone devices and add .gitignore

--- frontend/public/js/app.js
+++ frontend/public/js/app.js
@@ -1,13 +1,24 @@
 import { socket } from "./webrtc/signalingClient.js";
-
 import { getDeviceName } from "./utils/deviceId.js";
+import { renderDevices } from "./ui/deviceList.js";
 
 const myDeviceName = getDeviceName();
 
-socket.emit("register-device", {
-  name: myDeviceName,
+function updateLocalName() {
+  const nameElement = document.getElementById("myDeviceName");
+  if (nameElement) {
+    nameElement.textContent = myDeviceName;
+  }
+}
+
+socket.on("connect", () => {
+  socket.emit("register-device", {
+    name: myDeviceName,
+  });
 });
 
 socket.on("devices", (devices) => {
-  console.log(devices);
+  renderDevices(devices);
 });
+
+updateLocalName();
--- frontend/public/js/utils/deviceId.js
+++ frontend/public/js/utils/deviceId.js
@@ -1,8 +1,22 @@
+function getDefaultDeviceName() {
+  const ua = navigator.userAgent;
+
+  if (ua.includes("Android")) return "Android Device";
+
+  if (ua.includes("iPhone")) return "iPhone";
+
+  if (ua.includes("Windows")) return "Windows PC";
+
+  if (ua.includes("Mac")) return "Mac Device";
+
+  return "Browser Device";
+}
+
 export function getDeviceName() {
   let name = localStorage.getItem("deviceName");
 
   if (!name) {
-    name = "Chrome Device";
+    name = getDefaultDeviceName();
 
     localStorage.setItem("deviceName", name);
   }
@@ -10,6 +24,11 @@ export function getDeviceName() {
   return name;
 }
 
-export function saveDeviceName(name) {
+function saveDeviceName(name) {
   localStorage.setItem("deviceName", name);
 }
+
+window.DeviceId = {
+  getDeviceName,
+  saveDeviceName,
+};
